6e5f1683f37df03210a98810df275c0fbcd4d9a0
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2025-03-11 12:46:54 -0400

    added route for parsing data. Also added input fields and input validation for symbols.

--- application/route.py
+++ application/route.py
@@ -1,6 +1,26 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, jsonify
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/parse_data", methods=['POST'])
+def parse_data():
+    data = request.json
+    symbol = data.get('symbol')
+    timefame = data.get('choice')
+    options = data.get('api_response')
+    
+    for element in options['bestMatches']:
+        if element['4. region'] == 'United States':
+            symbol = element['1. symbol']
+            break
+    print(symbol, timefame)
+        
+    return render_template("graph.html")
+        
+@app.route("/graph")
+def graph(symbol, timeframe):
+    return
+    
